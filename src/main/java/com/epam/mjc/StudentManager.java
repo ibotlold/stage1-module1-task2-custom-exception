9a2387f1d6dea8c1dad84633173f18870571eb49
Kirill <ibotlold@gmail.com>
2023-11-06 22:10:22 +0600

    feat: solution

--- src/main/java/com/epam/mjc/StudentManager.java
+++ src/main/java/com/epam/mjc/StudentManager.java
@@ -5,8 +5,15 @@ public class StudentManager {
 
   private static final long[] IDs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 
-  public Student find(long studentID) {
-    return Student.getValueOf(studentID);
+  public Student find(long studentID) throws StudentNotFoundException {
+
+    Student result = Student.getValueOf(studentID);
+
+    if (result == null) {
+      throw new StudentNotFoundException(studentID);
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
